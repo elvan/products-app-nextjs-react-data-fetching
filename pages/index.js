9ff792079aaea8eb3634b2d142da9494db322748
Elvan Hidayat <elvanhd@gmail.com>
2023-04-20 01:07:35 +0700

    running server-side code

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,6 @@
+import fs from 'fs/promises';
+import path from 'path';
+
 import Link from 'next/link';
 
 function HomePage(props) {
@@ -15,26 +18,28 @@ function HomePage(props) {
 }
 
 export async function getStaticProps(context) {
+  console.log('(Re-)Generating...');
+  const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json');
+  const jsonData = await fs.readFile(filePath);
+  const data = JSON.parse(jsonData);
+
+  if (!data) {
+    return {
+      redirect: {
+        destination: '/no-data',
+      },
+    };
+  }
+
+  if (data.products.length === 0) {
+    return { notFound: true };
+  }
+
   return {
     props: {
-      products: [
-        {
-          id: 'p1',
-          title: 'Product 1',
-          description: 'This is product 1',
-        },
-        {
-          id: 'p2',
-          title: 'Product 2',
-          description: 'This is product 2',
-        },
-        {
-          id: 'p3',
-          title: 'Product 3',
-          description: 'This is product 3',
-        },
-      ],
+      products: data.products,
     },
+    revalidate: 10,
   };
 }
 
